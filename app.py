426d367c26b33d8dc17249c1fa16d461c1e29aaa
delliana <dellianasalsabilap3@gmail.com>
2023-05-06 15:27:38 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# Import Flask
+from flask import Flask, render_template
+import os
+import socket
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Register a route
+@app.route("/my-profile")
+def index():
+    return render_template('index.html')
+
+# Run the app
+if __name__ == "__main__":
+
+    # Start the Flask app
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
